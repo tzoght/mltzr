8b75f9cce5c84c7a88c6b0738c3b558bba2bfc14
Tony Zoght <tony@zoght.com>
2022-12-27 11:12:52 -0800

    build: towards mvp

---
+++ R/eda.R
@@ -0,0 +1,16 @@
+library(dplyr)
+
+#' Performs basic exploratory data analysis
+#'
+#' @param df A dataframe
+#'
+#' @return a char vector
+#' @export
+#'
+#' @examples
+print_basic_eda <- function(df) {
+  # g <- glimpse(df)
+  df |>
+     select_if(is.numeric) |>
+     summary()
+}
